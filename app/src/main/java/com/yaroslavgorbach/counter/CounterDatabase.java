f681271c7114473e58159d03427bcb7263f20d13
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2020-05-01 12:23:10 +0300

    created Repo

--- app/src/main/java/com/yaroslavgorbach/counter/CounterDatabase.java
+++ app/src/main/java/com/yaroslavgorbach/counter/CounterDatabase.java
@@ -1,10 +1,13 @@
 package com.yaroslavgorbach.counter;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Counter.class, version = 1)
 public abstract class CounterDatabase extends RoomDatabase {
@@ -18,6 +21,7 @@ public abstract class CounterDatabase extends RoomDatabase {
 
             sInstance = Room.databaseBuilder(context.getApplicationContext(),CounterDatabase.class, "counter.db")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
 
@@ -26,5 +30,37 @@ public abstract class CounterDatabase extends RoomDatabase {
     }
 
 
+    public static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
 
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+
+            super.onCreate(db);
+            new PopulateDbAsyncTask(sInstance).execute();
+        }
+
+    };
+
+    public static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private CounterDao counterDao;
+
+        private PopulateDbAsyncTask(CounterDatabase db) {
+
+            this.counterDao = db.counterDao();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            counterDao.insert(new Counter("test",1, 10, 22, 1, "test"));
+            counterDao.insert(new Counter("test",1, 10, 22, 1, "test"));
+            counterDao.insert(new Counter("test",1, 10, 22, 1, "test"));
+            counterDao.insert(new Counter("test",1, 10, 22, 1, "test"));
+            counterDao.insert(new Counter("test",1, 10, 22, 1, "test"));
+
+            return null;
+        }
+    }
 }
---
+++ app/src/main/java/com/yaroslavgorbach/counter/Repo.java
@@ -0,0 +1,96 @@
+package com.yaroslavgorbach.counter;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class Repo {
+
+   private CounterDao mCounterDao;
+   private LiveData<List<Counter>> mAll_counters;
+
+   public Repo(Application application){
+
+       CounterDatabase database = CounterDatabase.getInstance(application);
+        mCounterDao = database.counterDao();
+        mAll_counters = mCounterDao.getAllCounters();
+   }
+
+    public void insert(Counter counter){
+
+     new InsertCounterAsyncTask(mCounterDao).execute(counter);
+    }
+
+    public void delete(Counter counter){
+
+       new DeleteCounterAsyncTask(mCounterDao).execute(counter);
+    }
+
+    public void update(Counter counter){
+
+       new UpdateCounterAsyncTask(mCounterDao).execute(counter);
+    }
+
+    public LiveData<List<Counter>> getAllCounters(){
+
+       return mAll_counters;
+    }
+
+    public static class InsertCounterAsyncTask extends AsyncTask<Counter, Void, Void>{
+
+        private CounterDao counterDao;
+
+        private InsertCounterAsyncTask(CounterDao counterDao) {
+
+            this.counterDao = counterDao;
+
+        }
+
+        @Override
+        protected Void doInBackground(Counter... counters) {
+
+            counterDao.insert(counters[0]);
+            return null;
+        }
+    }
+
+    public static class DeleteCounterAsyncTask extends AsyncTask<Counter, Void, Void>{
+
+        private CounterDao counterDao;
+
+        private DeleteCounterAsyncTask(CounterDao counterDao) {
+
+            this.counterDao = counterDao;
+
+        }
+
+        @Override
+        protected Void doInBackground(Counter... counters) {
+
+            counterDao.delete(counters[0]);
+            return null;
+        }
+    }
+
+    public static class UpdateCounterAsyncTask extends AsyncTask<Counter, Void, Void>{
+
+        private CounterDao counterDao;
+
+        private UpdateCounterAsyncTask(CounterDao counterDao) {
+
+            this.counterDao = counterDao;
+
+        }
+
+        @Override
+        protected Void doInBackground(Counter... counters) {
+
+            counterDao.update(counters[0]);
+            return null;
+        }
+    }
+
+}
