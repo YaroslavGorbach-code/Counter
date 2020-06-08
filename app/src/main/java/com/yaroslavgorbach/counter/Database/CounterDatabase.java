169bb0f43656cdf02fccd49a19eedfcd6d00d7ed
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2020-06-08 09:43:30 +0300

    when database first created the 1 counter already exist

--- app/src/main/java/com/yaroslavgorbach/counter/Database/CounterDatabase.java
+++ app/src/main/java/com/yaroslavgorbach/counter/Database/CounterDatabase.java
@@ -1,14 +1,23 @@
 package com.yaroslavgorbach.counter.Database;
 
 import android.content.Context;
+import android.os.AsyncTask;
+
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.yaroslavgorbach.counter.Database.Daos.CounterDao;
 import com.yaroslavgorbach.counter.Database.Daos.CounterHistoryDao;
 import com.yaroslavgorbach.counter.Models.Counter;
 import com.yaroslavgorbach.counter.Models.CounterHistory;
+import com.yaroslavgorbach.counter.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @Database(entities = {Counter.class, CounterHistory.class },  version = 21)
 public abstract class CounterDatabase extends RoomDatabase {
@@ -25,10 +34,39 @@ public abstract class CounterDatabase extends RoomDatabase {
 
             sInstance = Room.databaseBuilder(context.getApplicationContext(),CounterDatabase.class, "counter.db")
                     .fallbackToDestructiveMigration()
+                    .addCallback(rdc)
                     .build();
         }
 
         return sInstance;
 
     }
+    private static RoomDatabase.Callback rdc = new RoomDatabase.Callback() {
+        public void onCreate(SupportSQLiteDatabase db) {
+            new PopulateAsyncTask(sInstance).execute();
+        }
+
+        public void onOpen(SupportSQLiteDatabase db) {
+            // do something every time database is open
+        }
+    };
+
+    private static class PopulateAsyncTask extends AsyncTask<Void, Void, Void>{
+        private CounterDao mDao;
+        PopulateAsyncTask(CounterDatabase db){
+            mDao = db.counterDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Date currentDate = new Date();
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.YY HH:mm:ss", Locale.getDefault());
+            String date = dateFormat.format(currentDate);
+
+            mDao.insert(new Counter("New counter",0, Long.parseLong("9999999999999999"), Long.parseLong("-9999999999999999"),
+                    1, null, date ));
+            return null;
+        }
+    }
+
 }
