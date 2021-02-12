f678801523d2fe6b179902355a092117727b0dde
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-02-12 19:45:20 +0200

    created toolbar for SettingsActivity

--- app/src/main/java/com/yaroslavgorbach/counter/Activityes/MainActivity.java
+++ app/src/main/java/com/yaroslavgorbach/counter/Activityes/MainActivity.java
@@ -20,24 +20,18 @@ public class MainActivity extends AppCompatActivity {
     private boolean mAllowedVolumeButtons;
     private SharedPreferences mSharedPreferences;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (mSharedPreferences.getBoolean("nightMod", false)){
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        }else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        }
-
+        setTheme(mSharedPreferences);
         setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        /* depending on pref keeps screen on or let it off*/
         mAllowedVolumeButtons = mSharedPreferences.getBoolean("useVolumeButtons", false);
         if (mSharedPreferences.getBoolean("keepScreenOn", true)) {
             MainActivity.this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -66,5 +60,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    /* depending on pref sets theme*/
+    private void setTheme(SharedPreferences sharedPreferences) {
+        if (sharedPreferences.getBoolean("nightMod", false)){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
--- app/src/main/java/com/yaroslavgorbach/counter/Activityes/SettingsActivity.java
+++ app/src/main/java/com/yaroslavgorbach/counter/Activityes/SettingsActivity.java
@@ -1,24 +1,29 @@
 package com.yaroslavgorbach.counter.Activityes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import com.yaroslavgorbach.counter.Fragments.SettingsFragment;
 import com.yaroslavgorbach.counter.R;
 
 public class SettingsActivity extends AppCompatActivity  {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
+        mToolbar = findViewById(R.id.toolbar_settings);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        mToolbar.setNavigationOnClickListener(v -> finish());
     }
 
 }
